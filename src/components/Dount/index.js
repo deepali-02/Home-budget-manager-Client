cf438b71804518f9ad658ece6a31e73c70438466
deepali-02 <deepali93.redekar@gmail.com>
2022-01-21 10:27:37 +0100

    fix bug in Dount component

--- src/components/Dount/index.js
+++ src/components/Dount/index.js
@@ -10,7 +10,6 @@ import { selectUser } from "../../store/user/selector";
 export default function Dount() {
   const myExpense = useSelector(selectMyExpenses);
   const { budget } = useSelector(selectUser);
-  const balance = "balance left";
 
   console.log("All data of myExpense selector", myExpense);
 
@@ -28,15 +27,23 @@ export default function Dount() {
     return ex.amount;
   });
   console.log("amount ", amt);
+
+  let sum = amt.reduce((a, b) => a + b, 0);
+  console.log("sum", sum);
+
+  const bal = "Balance left";
+  let balance = budget - sum;
+  console.log("balance", balance);
+  // const balance = 400;
   const expense = {
-    labels: [...categoryName, balance],
+    labels: [...categoryName, bal],
     datasets: [
       {
         label: "Monthy Expense",
         backgroundColor: [...categoryColor, "#808080"],
         borderColor: "rgba(0,0,0,1)",
         borderWidth: 2,
-        data: [...amt, budget],
+        data: [...amt, balance],
       },
     ],
   };
@@ -57,7 +64,7 @@ export default function Dount() {
 
   return (
     <div className="dountChart">
-      <Card className="mt-5" style={{width:"100%",display:"flex"}}>
+      <Card className="mt-5" style={{ width: "100%", display: "flex" }}>
         <Doughnut data={expense} options={options} />
       </Card>
     </div>
