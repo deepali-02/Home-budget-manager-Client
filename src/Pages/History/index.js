7724e325510a56eea7aa8f7f3925fef49e4b2ebc
deepali-02 <deepali93.redekar@gmail.com>
2022-01-29 17:37:21 +0100

    add code to show selected month expenses

--- src/Pages/History/index.js
+++ src/Pages/History/index.js
@@ -2,20 +2,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectCategory } from "../../store/myExpenses/selector";
 import { selectMyExpenses } from "../../store/myExpenses/selector";
 import BarChart1 from "../../components/BarChart";
-import { Image } from "react-bootstrap";
+import { Card, Form, Image, Container, Button } from "react-bootstrap";
 import "./style.css";
 import { useEffect } from "react";
 import { fetchMyExpenses } from "../../store/myExpenses/action";
 import { selectUser } from "../../store/user/selector";
 import { useNavigate } from "react-router";
 import testImg from "../../images/test.png";
+import SearchMonth from "../../components/SearchByMonth";
+import { selectSearchMonth } from "../../store/myExpenses/selector";
 
 export default function History() {
   const dispatch = useDispatch();
   const { id, token } = useSelector(selectUser);
   const navigate = useNavigate();
-  const ctegory = useSelector(selectCategory);
+
   const expense = useSelector(selectMyExpenses);
+  const monthExpense = useSelector(selectSearchMonth);
   console.log("Expenses from history", expense);
 
   useEffect(() => {
@@ -28,47 +31,87 @@ export default function History() {
   }, [dispatch, id, navigate, token]);
   return (
     <div>
-      {expense.length === 0 ? (
-        <div>
-          <Image
-            src={testImg}
-            style={{ width: "50%" }}
-            fluid
-            alt="No History image"
-          />
-          <h2>Please add expenses to see History</h2>
-        </div>
-      ) : (
-        <div>
-          <h1>History page</h1>
-          <div className="table-container">
-            <table>
-              <tr>
-                <th>Logo</th>
-                <th>Category</th>
-                <th>Amount</th>
-                <th>Date</th>
-              </tr>
-              {expense.map((ex) => (
-                <tr>
-                  <td>
-                    <img class="image" src={ex.category.logoUrl} alt="" />
-                  </td>
-                  <td>{ex.category.name}</td>
-                  <td>{ex.amount}</td>
-                  <td>{ex.date}</td>
-                </tr>
-              ))}
-            </table>
-          </div>
+      <div>
+        {expense.length === 0 ? (
           <div>
-            <h3> Expense history as per categories</h3>
-            <div style={{ marginLeft: "20%", marginTop: "5%" }}>
-              <BarChart1 />
+            <Image
+              src={testImg}
+              style={{ width: "50%" }}
+              fluid
+              alt="No History image"
+            />
+            <h2>Please add expenses to see History</h2>
+          </div>
+        ) : (
+          <div>
+            <h1>History page</h1>
+            <div>
+              <Container className="mt-5 mb-5" style={{ width: "60rem" }}>
+                <SearchMonth />
+              </Container>
+            </div>
+            <div className="table-container">
+              {monthExpense.length !== 0 ? (
+                <>
+                  <table>
+                    <tr>
+                      <th>Logo</th>
+                      <th>Category</th>
+                      <th>Amount</th>
+                      <th>Date</th>
+                      <th> </th>
+                    </tr>
+                    {monthExpense.map((ex) => (
+                      <tr>
+                        <td>
+                          <img class="image" src={ex.category.logoUrl} alt="" />
+                        </td>
+                        <td>{ex.category.name}</td>
+                        <td>{ex.amount}</td>
+                        <td>{ex.date}</td>
+                        <td>
+                          <Button variant="danger">delete</Button>
+                        </td>
+                      </tr>
+                    ))}
+                  </table>
+                </>
+              ) : (
+                <>
+                  <table>
+                    <tr>
+                      <th>Logo</th>
+                      <th>Category</th>
+                      <th>Amount</th>
+                      <th>Date</th>
+                      <th> </th>
+                    </tr>
+                    {expense.map((ex) => (
+                      <tr>
+                        <td>
+                          <img class="image" src={ex.category.logoUrl} alt="" />
+                        </td>
+                        <td>{ex.category.name}</td>
+                        <td>{ex.amount}</td>
+                        <td>{ex.date}</td>
+                        <td>
+                          <Button variant="danger">delete</Button>
+                        </td>
+                      </tr>
+                    ))}
+                  </table>
+                </>
+              )}
+            </div>
+            <div>
+              <h3> Expense history as per categories</h3>
+              <div style={{ marginLeft: "20%", marginTop: "5%" }}>
+                <BarChart1 />
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
