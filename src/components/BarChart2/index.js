4805c6b6d6528206f81675b63629609d7b8fdf08
deepali-02 <deepali93.redekar@gmail.com>
2022-02-02 12:01:34 +0100

    create new BarChart2 component

---
+++ src/components/BarChart2/index.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Bar } from "react-chartjs-2";
+import { BarChart } from "recharts";
+import { MDBContainer } from "mdbreact";
+
+// import { useNavigate } from "react-router";
+import { selectSearchMonth } from "../../store/myExpenses/selector";
+import { selectMyExpenses } from "../../store/myExpenses/selector";
+// import { selectUser } from "../../store/user/selector";
+
+export default function BarChart2() {
+  const myExpense = useSelector(selectMyExpenses);
+  const monthExpense = useSelector(selectSearchMonth);
+  //     const { budget, token } = useSelector(selectUser);
+  //   const navigate = useNavigate();
+  let mergedCategories;
+
+  if (monthExpense.length !== 0) {
+    mergedCategories = monthExpense.reduce((acc, expense) => {
+      const categoryExist = acc.find(
+        (eachExpense) => eachExpense.categoryId === expense.categoryId
+      );
+      // console.log("Category Exist", categoryExist);
+      const updatedCategory = categoryExist
+        ? { ...categoryExist, amount: categoryExist.amount + expense.amount }
+        : null;
+      const newAcc = updatedCategory
+        ? acc.map((eachExpense) => {
+            if (
+              parseInt(eachExpense.categoryId) ===
+              parseInt(updatedCategory.categoryId)
+            ) {
+              return updatedCategory;
+            } else {
+              return eachExpense;
+            }
+          })
+        : [...acc, expense];
+      return newAcc;
+    }, []);
+  } else {
+    mergedCategories = myExpense.reduce((acc, expense) => {
+      const categoryExist = acc.find(
+        (eachExpense) => eachExpense.categoryId === expense.categoryId
+      );
+      // console.log("Category Exist", categoryExist);
+      const updatedCategory = categoryExist
+        ? { ...categoryExist, amount: categoryExist.amount + expense.amount }
+        : null;
+      const newAcc = updatedCategory
+        ? acc.map((eachExpense) => {
+            if (
+              parseInt(eachExpense.categoryId) ===
+              parseInt(updatedCategory.categoryId)
+            ) {
+              return updatedCategory;
+            } else {
+              return eachExpense;
+            }
+          })
+        : [...acc, expense];
+      return newAcc;
+    }, []);
+  }
+
+  const categoryName = mergedCategories.map((nm) => nm.category.name);
+  console.log("Categories Name", categoryName);
+
+  const amt = mergedCategories.map((ex) => {
+    return ex.amount;
+  });
+  console.log("Amount from bar2", amt);
+
+  const data = {
+    labels: categoryName,
+    datasets: [
+      {
+        label: "Hours Studied in Geeksforgeeks",
+        data: amt,
+        backgroundColor: "#02b844",
+        borderWidth: 1,
+        borderColor: "#000000",
+      },
+    ],
+  };
+
+  return (
+    <MDBContainer>
+      {/* <BarChart barSize={20}> */}
+      <Bar data={data} style={{ maxHeight: "600px" }} />
+      {/* </BarChart> */}
+    </MDBContainer>
+  );
+}
