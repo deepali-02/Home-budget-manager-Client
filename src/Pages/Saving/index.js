3c1ac4ed8333882cd63c4f86bc80d89e3bb1b2f7
deepali-02 <deepali93.redekar@gmail.com>
2022-03-01 20:12:41 +0100

    update  saving page

--- src/Pages/Saving/index.js
+++ src/Pages/Saving/index.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import { Col, Image, Row, Container, Button } from "react-bootstrap";
+import { Col, Image, Row, Container, Button, Card } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchGoal } from "../../store/Goal/action";
 import { selectGoals } from "../../store/Goal/selector";
 import { selectToken } from "../../store/user/selector";
-import moment from "moment";
+// import moment from "moment";
 
 import testImg from "../../images/test.png";
 import { Link } from "react-router-dom";
@@ -46,64 +46,66 @@ export default function Saving() {
           </Row>
         </Container>
       ) : (
-        <Container fluid>
+        <Container>
           <Row className="mb-5 ">
             <Col>
               <Button
                 variant="warning"
                 style={{ position: "right" }}
-                onClick={() => navigate("/addSavings")}
+                onClick={() => navigate("/addSavingsGoal")}
               >
                 Set saving goals
               </Button>
             </Col>
           </Row>
+          <Row xs={1} md={3} >
           {goals.map((goal) => (
-            <Row
-              // sx={4}
-              // md={4}
-              // lg={6}
-              className="mb-3 mt-3 justify-content-md-center"
-              style={{
-                borderStyle: "groove",
-                backgroundColor: "#82EEFD",
-                height: "5rem",
-                marginLeft: "10%",
-                marginRight: "10%",
-              }}
-            >
-              <Col
-                className="mt-3"
-                style={{ textAlign: "left", backgroundColor: "#82EEFD" }}
+            <>
+              {/* <Row
+                xs={2}
+                md={4}
+                lg={6}
+                // md={4}
+                // lg={6}
+                // className="justify-content-md-center"
+                style={{
+                  borderStyle: "groove",
+                  backgroundColor: "#82EEFD",
+                  // height: "5rem",
+                  // marginLeft: "10%",
+                  // marginRight: "50%",
+                }}
               >
-                <b>{goal.goal_name}</b>
-              </Col>
-              <Col className="mt-3" style={{ backgroundColor: "#82EEFD" }}>
-                <b>Target: {goal.target_amount}€</b>
-              </Col>
-              <Col className="mt-3" style={{ backgroundColor: "#82EEFD" }}>
-                <b>
-                  Desire date: {moment(goal.desire_date).format("DD/MM/YYYY")}
-                </b>
-              </Col>
-              <Col className="mt-3">
-                <b>Saved: {goal.saved_amount}</b>
-              </Col>
-              <Col
-                className="mt-3"
-                style={{ backgroundColor: "#82EEFD", textAlign: "right" }}
-              >
-                {/* {console.log("goal id =", goal.id)} */}
-                <Link to={`/detail_savings/${goal.id}`}>
-                  <Button
-                    style={{ borderRadius: "50%", justifyContent: "right" }}
-                  >
-                    <h3>+</h3>
-                  </Button>
-                </Link>
-              </Col>
-            </Row>
+                <Col style={{ textAlign: "right", backgroundColor: "#82EEFD" }}>
+                  <b>{goal.goal_name}</b>
+                </Col>
+
+                <Col style={{ backgroundColor: "#82EEFD", textAlign: "right" }}>
+                
+                  <Link to={`/detail_savings/${goal.id}`}>
+                    <Button
+                      style={{ borderRadius: "50%", justifyContent: "right" }}
+                    >
+                      <h3>+</h3>
+                    </Button>
+                  </Link>
+                </Col>
+              </Row> */}
+              <Container>
+                <Col>
+                <Card className="mb-5" bg="info" style={{ width: "18rem" }}>
+                  <Card.Body>
+                    <Card.Title>{goal.goal_name}</Card.Title>
+                    <Link to={`/detail_savings/${goal.id}`}>
+                      <Button variant="primary">Show details</Button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+                </Col>
+              </Container>
+            </>
           ))}
+          </Row>
         </Container>
       )}
     </>
